52546b63d337e71aa56dbbb48f74e5be31dcb112
reshmeyadav <reshmeyadav10@gmail.com>
2025-12-02 20:40:26 +0530

    Merged text and Image route in One route

--- src/main/java/com/example/demo/controller/AppointmentController.java
+++ src/main/java/com/example/demo/controller/AppointmentController.java
@@ -5,9 +5,12 @@ import com.example.demo.dto.AppointmentResponse;
 import com.example.demo.service.AppoinmentService;
 import com.example.demo.service.OcrService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 import static org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE;
 
@@ -19,18 +22,18 @@ public class AppointmentController {
     private final OcrService ocrService;
     private final AppoinmentService appoinmentService;
 
-    @PostMapping("/parse")
+    @PostMapping(value = "/parse", consumes = MediaType.APPLICATION_JSON_VALUE)
     public AppointmentResponse parseAppointment(@RequestBody AppointmentRequest request) {
         return appoinmentService.processText(request.getText());
     }
 
-    @PostMapping(value = "/parse-file", consumes = MULTIPART_FORM_DATA_VALUE)
-    public String parseFile(@RequestParam("file")MultipartFile file) {
+    @PostMapping(value = "/parse", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public AppointmentResponse parseFile(@RequestParam("file")MultipartFile file) {
         try {
             String text = ocrService.extractTextFromStream(file.getInputStream());
-            return text;
-        } catch (java.io.IOException e) {
-            return "Some error occurs";
+            return appoinmentService.processText(text);
+        } catch (IOException e) {
+            throw new RuntimeException("Error while doing OCR"+ e);
         }
     }
 }
